f5cc95a53be70ffc253bf149467983a63fad5c1e
Prajwal P B <156583151+csprajwalpb@users.noreply.github.com>
2025-01-12 10:50:27 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "B. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Markup Language",
+            "B. Hyper Text Markup Language",
+            "C. Home Text Multiple Language",
+            "D. Hyper Type Multiple Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Code Style Sheet",
+            "D. Computer Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does SQL stand for?",
+        answer: "B. Structured Query Language",
+        options: [
+            "A. Structural Query Language",
+            "B. Structured Query Language",
+            "C. Shape Query Language",
+            "D. Silent Query Language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does PHP stand for?",
+        answer: "D. HyperText Preprocessor",
+        options: [
+            "A. HomeText Programming",
+            "B. HyperText Process",
+            "C. HyperText Programming",
+            "D. HyperText Preprocessor"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "C. Extensible Markup Language",
+        options: [
+            "A. Explore Multiple Language",
+            "B. Extensive Markup Language",
+            "C. Extensible Markup Language",
+            "D. Extra Markup Language"
+        ]
+    }
+];
